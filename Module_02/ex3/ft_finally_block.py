1c6d01564fde0fa32dad5d54af874a1945326aa8
anait-il <abdelkabir@student.42.fr>
2026-02-02 19:11:40 +0100

    test3

--- Module_02/ex3/ft_finally_block.py
+++ Module_02/ex3/ft_finally_block.py
@@ -3,12 +3,48 @@
 def water_plants(plant_list: list) -> None:
     print("Opening watering system")
     try:
-        if plant_list == None:
+        if plant_list is None:
             raise TypeError("Cannot water None - invalide plant!")
         for plant in plant_list:
-            if 
+            if plant is None:
+                raise ValueError("Cannot water None - invalid plant!")
             print(f"Watering {plant}")
+    except ValueError as e:
+        print(f"Error: {e}")
+        raise ValueError("Error")
+    
+    finally:
+        print("Closing watering system (cleanup)")
+
+
+
+def	test_watering_system() -> None:
+    print("=== Garden Watering System ===")	
+    try:
+        print("\nTesting normal watering...")
+        plant: list = ["tomato", "lettuce", "carrots"]
+        water_plants(plant)
     except:
-        
+        pass
+    else:
+        print("Watering completed successfully")
+
+    try:
+        print("\nTesting with error...")
+        plant2: list = ["tomato", None, "botato"]
+        water_plants(plant2)
+    except:
+        pass
+    else:
+        print("Watering completed successfully")
+    
+    finally:
+        print("\nCleanup always happens, even with errors!")
+
+
+
+test_watering_system()
+
+		
 
     
