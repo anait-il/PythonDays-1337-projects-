3aba9b78579f4a32334ad9e2d611be0b9d1a419f
Abdelkabir Nait Ilyasse <anait-il@e2r5p11.1337.ma>
2026-02-15 00:23:10 +0100

    test

--- Module_03/ex6/ft_analytics_dashboard.py
+++ Module_03/ex6/ft_analytics_dashboard.py
@@ -1,7 +1,101 @@
+players: dict = {
+    "alice": {'score' : 2300,
+              'status' : 'active',
+              'achievements' : ('killed monster',
+                               'found treasure',
+                               'leveled up',
+                               'treasure_seeker',
+                               'boss_hunter',
+                               'killed monster'),
+              'region' : "north" 
+             },
+    "bob":   {'score' : 1850,
+              'status' : 'active',
+              'achievements' : ('killed monster',
+                                'combo_king',
+                                'explorer'),
+              'region' : "east" 
+             },
+    "charlie":   {'score' : 2100,
+              'status' : 'active',
+              'achievements' : ('leveled up',
+                                'treasure_seeker'),
+              'region' : "central" 
+             },
+    "diana":   {'score' : 500,
+              'status' : 'inactive',
+              'achievements' : ('found treasure',),
+              'region' : "west" 
+             }
+}
+
+
 def list_comp() -> None:
     print("=== List Comprehension Examples ===")
-    
+    hight_score: list = [player for player in players if players[player]['score'] > 2000]
+    print(f"Hight scores (>2000): {hight_score}")
+
+    scores_doubled: list = [players[player]['score'] * 2 for player in players]
+    print(f"Scores doubled: {scores_doubled}")
+
+    active_palyers: list = [player for player in players if players[player]['status'] == 'active']
+    print(f"Active players: {active_palyers}\n")
+
+
+def dict_comp() -> None:
+    players_score: dict = {player: players[player]['score'] for player in  players}
+    print(f"Player scores: {players_score}")
+
+    categories: list = ['hight' if players[player]['score'] > 2000
+                         else 'medium' if players[player]['score'] > 1500
+                         else 'low'
+                         for player in players]
+    score_categorie: dict = {categorie: sum(1 for c in categories if c == categorie) for categorie in categories}
+    print(f"Score categorie: {score_categorie}")
+
+    achievements_count: dict = {player: len(players[player]['achievements']) for player in players}
+    print(f"Achievements regions: {achievements_count}")
+
+
+def set_comp() -> None:
+    unique_players: set = {player for player in players}
+    print(f"Unique players: {unique_players}")
+
+    score_achiev: set = {players[player]['achievements'] for player in players}
+    print(f"Unique achievements: {score_achiev}")
+
+    active_regions: set = {players[player]['region'] for player in players if players[player]['status'] == 'active'}
+    print(f"Active players: {active_regions}")
+
+
+def analytics() -> None:
+    total_players: int = len(players)
+    print(f"Total players: {total_players}")
+
+    achievements: set = {players[player]['achievements'] for player in players}
+    total_unique_achievments: int = len({achievements})
+    print(f"Total unique achievements: {total_unique_achievments}")
+
+    scores: list = [players[player]['score'] for player in players]
+    average_score: int = len(scores) / total_players
+    print(f"Average scores: {average_score}")
+
+    top_perform: int = max(scores)
+    print(f"Top performer: {[player for player in players if players[player]['score'] == top_perform]}")
 
 
 def main() -> None:
     print("=== Game Analytics Dashboard ===\n")
+    list_comp()
+
+    print("=== Dict Comprehension Examples ===")
+    dict_comp()
+
+    print("\n=== Set Comprehension Examples ===")
+    set_comp()
+
+    print("\n=== Combined Analysis ===")
+    analytics()
+
+if __name__ == "__main__":
+    main()
---
+++ Module_03/test.py
@@ -0,0 +1,8 @@
+class A:
+    def __contains__(self, str):
+        print('hello')
+
+obj = A()
+print('faisal' in obj)
+
+
