ed426640acfa6209a3b05ef0c222943ebcd8ed0f
anait-il <abdelkabir@student.42.fr>
2026-02-14 16:39:42 +0100

    test

--- Module_03/ex4/ft_inventory_system.py
+++ Module_03/ex4/ft_inventory_system.py
@@ -5,16 +5,16 @@ import sys
 def current_inventory(dic: dict, total_item: int) -> dict:
     i: int = 0
     sort_dic: dict = dict()
-    tmp_dic: dict = {k: v for k, v in dic.items()}
-    
-    while i < len(tmp_dic):
+    new_dic = {k: v for k, v in dic.items()}
+
+    while i < len(new_dic):
         key: str = ""
         value: int = 0
-        for k, v in tmp_dic.items():
+        for k, v in new_dic.items():
             if v > value:
                 value = v
                 key = k
-        tmp_dic[key] = 0
+        new_dic[key] = 0
         sort_dic[key] = value
         i += 1
     for k, v in sort_dic.items():
@@ -23,55 +23,67 @@ def current_inventory(dic: dict, total_item: int) -> dict:
 
 
 if __name__ == "__main__":
-    print("=== Inventory System Analysis ===\n")
-    if len(sys.argv) > 1:
-        try:
-            dic: dict = dict()
-            i: int = 1
-            while i < len(sys.argv):
-                parts: list = sys.argv[i].split(":")
-                if len(parts) != 2 or not parts[1]:
+    try:
+        print("=== Inventory System Analysis ===")
+        if len(sys.argv) > 1:
+            try:
+                dic: dict = dict()
+                i: int = 1
+                while i < len(sys.argv):
+                    parts: list = sys.argv[i].split(":")
+                    if len(parts) != 2 or not parts[1]:
+                        i += 1
+                        print(f"Error: key '{parts[0]}' without value")
+                        continue
+                    dic[parts[0]] = int(parts[1])
                     i += 1
-                    print(f"Error: key '{parts[0]}' without value")
-                    continue
-                dic[parts[0]] = int(parts[1])
-                i += 1
-        except Exception as e:
-            print(f"Error: {e}\n")
+            except Exception as e:
+                print(f"Error: {e}\n")
 
-        total_item: int = 0
-        item: int = 0
-        for value in dic.values():
-            total_item += value
-            item += 1
-        print(f"Total items in inventory: {total_item}")
-        print(f"Unique item types: {item}")
+            total_item: int = 0
+            item: int = 0
+            for value in dic.values():
+                total_item += value
+                item += 1
+            print(f"Total items in inventory: {total_item}")
+            print(f"Unique item types: {item}")
 
-        print("\n=== Current Inventory ===")
-        print("######", dic)
-        sort_dict: dict = current_inventory(dic, total_item)
-        print("######", dic)
+            print("\n=== Current Inventory ===")
+            sort_dict: dict = current_inventory(dic, total_item)
 
-        print("\n=== Inventory Statistics ===")
-        keys: list = sort_dict.keys()
-        for key, value in sort_dict.items():
-            f_dic: dict = dict({key: value})
-            break
-        print(f"Most aabundant: {f_dic}")
-        for key, value in sort_dict.items():
-            lst_dic: dict = dict({key: value})
-        print(f"Least aabundant: {lst_dic}")
+            print("\n=== Inventory Statistics ===")
+            for key, value in sort_dict.items():
+                f_dic: dict = dict({key: value})
+                break
+            print(f"Most aabundant: {f_dic}")
+            for key, value in sort_dict.items():
+                lst_dic: dict = dict({key: value})
+            print(f"Least aabundant: {lst_dic}")
 
-        print("\n=== Management Suggestions ===")
-        restock_needed: list = []
-        for key, value in sort_dict.items():
-            if value == 1:
-                restock_needed += [key]
-        print(f"Restock needed: {restock_needed}")
+            print("\n=== Item Categories ===")
+            abundance_dictionary: dict = {"moderate": {}, "scarce": {}}
+            for k, v in sort_dict.items():
+                if v > 3:
+                    abundance_dictionary["moderate"].update({k: v})
+                elif v <= 3:
+                    abundance_dictionary["scarce"].update({k: v})
+            print(f"Moderate: {abundance_dictionary['moderate']}")
+            print(f"Scarce: {abundance_dictionary['scarce']}")
 
-        print("\n=== Dictionary Properties Demo ===")
-        print(f"Dictionary keys: {list(dic.keys())}")
-        print(f"Dictionary values: {list(dic.values())}")
-        inventory_search: str = "sword"
-        print(f"Sample lookup - '{inventory_search}'", end=" ")
-        print(f"in inventory: {dic[inventory_search]}")
+            print("\n=== Management Suggestions ===")
+            restock_needed: list = []
+            for key, value in sort_dict.items():
+                if value == 1:
+                    restock_needed += [key]
+            print(f"Restock needed: {restock_needed}")
+
+            print("\n=== Dictionary Properties Demo ===")
+            dic_keys: list = [key for key in dic.keys()]
+            dic_val: list = [value for value in dic.values()]
+            print(f"Dictionary keys: {dic_keys}")
+            print(f"Dictionary values: {dic_val}")
+            inventory_search: str = "sword"
+            print(f"Sample lookup - '{inventory_search}'", end=" ")
+            print(f"in inventory: {dic.get(inventory_search) == True}")
+    except Exception as e:
+        print(e)
--- Module_03/ex6/ft_analytics_dashboard.py
+++ Module_03/ex6/ft_analytics_dashboard.py
@@ -1,1 +1,7 @@
+def list_comp() -> None:
+    print("=== List Comprehension Examples ===")
+    
 
+
+def main() -> None:
+    print("=== Game Analytics Dashboard ===\n")
