b5ae3e6acdd01b5a802d72dbee10380f1c127efe
Abdelkabir Nait Ilyasse <anait-il@e2r5p11.1337.ma>
2026-02-15 19:38:25 +0000

    test

---
+++ Module_04/ex1/ft_archive_creation.py
@@ -0,0 +1,25 @@
+
+def main() -> None:
+    print("=== CYBER ARCHIVES - PRESERVATION SYSTEM ===\n")
+    file: str = 'new_discovery.txt'
+    try:
+        print("Initializing new storage unit: new_discovery.txt")
+        with open(file, 'w') as f:
+            print("Storage unit created successfully...\n")
+            print("Inscribing preservation data...")
+            f.write("[ENTRY 001] New quantum algorithm discovered\n")
+            f.write("[ENTRY 002] Efficiency increased by 347%\n")
+            f.write("[ENTRY 003] Archived by Data Archivist trainee\n")
+            
+        with open(file, 'r') as f:
+            print(f.read())
+        print("Data inscription complete. Storage unit sealed.")
+        print("Archive 'new_discovery.txt' ready for long-term preservation.")
+    except PermissionError as e:
+        print(f"Error: {e}")
+    except Exception:
+        print("ERROR: Storage vault not found.")      
+
+
+if __name__ == "__main__":
+    main()
---
+++ Module_04/ex2/ft_stream_management.py
@@ -0,0 +1,18 @@
+
+import sys
+
+def main() -> None:
+    sys.stdout.write("=== CYBER ARCHIVES - COMMUNICATION SYSTEM ===\n\n")
+    archivist_id: str = input("Input Stream active. Enter archivist ID: ")
+    status: str = input("Input Stream active. Enter status report: ")
+
+    sys.stdout.write(f"\n[STANDARD] Archive status from {archivist_id}: {status}\n")
+    sys.stderr.write(f"[ALERT] System diagnostic: Communication channels verified\n")
+    sys.stdout.write("[STANDARD] Data transmission complete\n")
+
+    sys.stdout.write("\n\nThree-channel communication test successful.\n")
+    
+
+
+if __name__ == "__main__":
+    main()
