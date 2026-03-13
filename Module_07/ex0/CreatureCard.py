fd923e14a45affc7f0df4c4527d3d7d860fc4711
Abdelkabir Nait Ilyasse <anait-il@e1r2p1.1337.ma>
2026-03-13 23:50:22 +0000

    strating 07

--- Module_07/ex0/CreatureCard.py
+++ Module_07/ex0/CreatureCard.py
@@ -1,6 +1,30 @@
 from ex0.Card import Card
+from typing import Dict
 
 
 class CreatureCard(Card):
     def __init__(self) -> None:
-        self.attack = 
+        self.__attack = 0
+        self.__health = 0
+
+    def play(self, game_state: dict) -> None:
+        pass
+
+    def attack_target(self) -> None:
+        pass
+
+    def set_attheal(self, attack, health) -> None:
+        if attack >= 0:
+            self.__attack = attack
+        else:
+            raise ValueError("attack is not valid")
+        if health >= 0:
+            self.__health = health
+        else:
+            raise ValueError("health is not valid")
+    
+    def get_atheal(self, attack, health) ->Dict[str, int]:
+        return {
+            "attack": self.__attack,
+            "health": self.__health
+        }
---
+++ Module_07/ex0/main.py
@@ -0,0 +1,6 @@
+from ex0.CreatureCard import CreatureCard
+
+
+def main() -> None:
+    print("\n=== DataDeck Card Foundation ===")
+    
